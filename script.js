7bb3ea92ee1841dd3fd4a0cc2b633f63778286c6
Szél Levente <szel.levente@diak.szbi-pg.hu>
2025-11-10 12:23:08 +0100

    save

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
 function switchTheme() {
   document.body.classList.add('bg-dark')
+
+  const navbar = document.getElementById('navbar')
+  const cards = document.querySelectorAll('.card')
+  const tables =  document.querySelectorAll('.table')
+  const cardColums = document.querySelectorAll(".column")
+  const footerText = document.querySelector(".navbar-text")
+  const h1Title = document.getElementById(".h1_title")
+  
+
+  navbar.classList.remove('navbar-light', 'bg-light')
+  navbar.classList.add('navbar-dark', 'bg-dark')
+ 
+
+  cards.forEach(card => {
+    card.classList.add("text-white", "bg-dark", "border-light")
+  })
+
+  tables.forEach(table => {
+    table.classList.add("table-dark")
+  })
+
+  cardColums.forEach(card => {
+    card.classList.remove("col");
+    card.classList.add("col-12");
+  })
+
+    footerText.style.float = 'right';
+    footerText.style.display = 'block';
+    footerText.classList.add('text-white');
+    h1Title.innerHTML="This is a dark theme";
+    h1Title.classList.add("text-white")
+
 }
