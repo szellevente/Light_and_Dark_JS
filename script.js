bc0c8b0d93fb4afd3d2efa05bd2c1af49b37a907
Szél Levente <szel.levente@diak.szbi-pg.hu>
2025-11-17 10:39:02 +0100

    save

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function switchTheme() {
+  if (!document.body.classList.contains("bg-dark"))
+  {
   document.body.classList.add('bg-dark')
 
   const navbar = document.getElementById('navbar')
@@ -6,15 +8,13 @@ function switchTheme() {
   const tables =  document.querySelectorAll('.table')
   const cardColums = document.querySelectorAll(".column")
   const footerText = document.querySelector(".navbar-text")
-  const h1Title = document.getElementById(".h1_title")
+  const h1Title = document.getElementById("h1_title")
+  const sw = document.getElementById("themeSwitch")
   
 
   navbar.classList.remove('navbar-light', 'bg-light')
   navbar.classList.add('navbar-dark', 'bg-dark')
  
-if (theme == false) {
-  
-}
 
   cards.forEach(card => {
     card.classList.add("text-white", "bg-dark", "border-light")
@@ -29,10 +29,55 @@ if (theme == false) {
     card.classList.add("col-12");
   })
 
-    footerText.style.float = 'right';
+    footerText.style.paddingLeft = '1000px';
     footerText.style.display = 'block';
     footerText.classList.add('text-white');
-    h1Title.innerHTML="This is a dark theme";
-    h1Title.classList.add("text-white")
+
+    
+    h1Title.innerHTML = "This is the Dark theme";
+    h1Title.classList.add('text-white');
+
+    sw.innerHTML = "Activate Light Theme";
+  }
+
+  else if(document.body.classList.contains("bg-dark"))
+{
+  document.body.classList.remove('bg-dark')
+
+  const navbar = document.getElementById('navbar')
+  const cards = document.querySelectorAll('.card')
+  const tables =  document.querySelectorAll('.table')
+  const cardColums = document.querySelectorAll(".column")
+  const footerText = document.querySelector(".navbar-text")
+  const h1Title = document.getElementById("h1_title")
+  const sw = document.getElementById("themeSwitch")
+
+  navbar.classList.add('navbar-light', 'bg-light')
+  navbar.classList.remove('navbar-dark', 'bg-dark')
+ 
+
+  cards.forEach(card => {
+    card.classList.remove("text-white", "bg-dark", "border-light")
+  })
+
+  tables.forEach(table => {
+    table.classList.remove("table-dark")
+  })
+
+  cardColums.forEach(card => {
+    card.classList.add("col");
+    card.classList.remove("col-12");
+  })
+
+    footerText.style.paddingLeft = '100px';
+    footerText.style.display = 'block';
+    footerText.classList.remove('text-white');
+
+    
+    h1Title.innerHTML = "This is the Light theme";
+    h1Title.classList.remove('text-white');
+
+    sw.innerHTML = "Activate Dark Theme";
+}
 
 }
